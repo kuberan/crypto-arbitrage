9859b6e45fb30f30f3a9cedd8d6c85b19d55a88b
Kuberan Marimuthu <kuberanmarimuthu@gmail.com>
2019-02-16 10:26:03 -0800

    Updated profitcalc.sh script for BNB

--- profitcalc.sh
+++ profitcalc.sh
@@ -9,3 +9,13 @@ grep 'Net Profit after fee: (' btc_backtest.file | sort -n | uniq | cut -d" " -f
 cat btc_profits.txt | sort -n
 echo "Total Profit (BTC)"
 cat ./btc_profits.txt | awk '{s+=$1} END {print s}'
+
+grep 'Net Profit after fee: (' eth_backtest.file | sort -n | uniq | cut -d" " -f6 > ./eth_profits.txt
+cat eth_profits.txt | sort -n
+echo "Total Profit (ETH)"
+cat ./eth_profits.txt | awk '{s+=$1} END {print s}'
+
+grep 'Net Profit after fee: (' bnb_backtest.file | sort -n | uniq | cut -d" " -f6 > ./bnb_profits.txt
+cat bnb_profits.txt | sort -n
+echo "Total Profit (BNB)"
+cat ./bnb_profits.txt | awk '{s+=$1} END {print s}'
