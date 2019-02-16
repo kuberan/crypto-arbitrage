9a959ef9681187ab4cc593a83f17ad7502c50afb
Kuberan Marimuthu <kuberanmarimuthu@gmail.com>
2019-02-16 10:03:54 -0800

    Update Profit Calc script for BTC

--- profitcalc.sh
+++ profitcalc.sh
@@ -2,5 +2,10 @@
 
 grep 'Net Profit after fee: (' usdt_backtest.file | sort -n | uniq | cut -d" " -f6 > ./usdt_profits.txt
 cat usdt_profits.txt | sort -n
-echo "Total"
+echo "Total Profit (USDT)"
 cat ./usdt_profits.txt | awk '{s+=$1} END {print s}'
+
+grep 'Net Profit after fee: (' btc_backtest.file | sort -n | uniq | cut -d" " -f6 > ./btc_profits.txt
+cat btc_profits.txt | sort -n
+echo "Total Profit (BTC)"
+cat ./btc_profits.txt | awk '{s+=$1} END {print s}'
