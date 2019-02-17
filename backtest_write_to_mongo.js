eab4f17874d4ebb0e0937c369b19207dc3f9ff66
Kuberan Marimuthu <kuberanmarimuthu@gmail.com>
2019-02-17 14:11:07 -0800

    Backtest Script version with write to mongoDB

---
+++ backtest_write_to_mongo.js
@@ -0,0 +1,25 @@
+var assert = require('assert');
+var MongoClient = require('mongodb').MongoClient;
+var arbitrageFunctions = require('./arbitrage_functions');
+
+//const url = 'mongodb://adminuser:oNsz2kXGlBiMd9S2@cluster0-shard-00-00-kokgx.mongodb.net:27017,cluster0-shard-00-01-kokgx.mongodb.net:27017,cluster0-shard-00-02-kokgx.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true';
+const url = 'mongodb://myUserAdmin:abc123@localhost:27017';
+const dbName = 'binance';
+const client = new MongoClient(url);
+
+client.connect(function(err, client) {
+	assert.equal(null, err);
+	const db = client.db(dbName);
+
+	// Insert a single document
+	var cursor = db.collection('binanceOrderBook').find({}).forEach(function(doc) {
+		arbitrageFunctions.findTriangularArbitrage(doc.timestamp,doc.binanceOrderData,checkOrderQuantity=false, profitThresholdPercent=0.0, baseCurrency='USDT', debug=false, feePercent=0.0, callback=function(response) {
+		db.collection('binanceBackTestResults').insertOne(response, function(){});
+	});
+	}, function(err) {
+		console.log(err);
+		client.close();
+	});
+});
+
+
