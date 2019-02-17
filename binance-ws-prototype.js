d5e00c4dc58d2b4f584f2c98beb2d2efeca72fc0
Kuberan Marimuthu <kuberanmarimuthu@gmail.com>
2019-02-17 14:53:26 -0800

    Binance WebSocket timestamp

--- binance-ws-prototype.js
+++ binance-ws-prototype.js
@@ -24,5 +24,7 @@ binanceWS.onDepthLevelUpdate('BTCUSDT',20, (data) => {
 	console.log('BTC/USDT bid qty: ' + data.bids[0][1]);
         console.log('BTC/USDT ask price: ' + data.asks[0][0]);
 	console.log('BTC/USDT ask qty: ' + data.asks[0][0]);
+	var ts = (new Date().getTime());
+	console.log('Human Readable Time: ' + (new Date(ts)).toLocaleDateString() + ' ' + (new Date(ts)).toLocaleTimeString());
 	console.log(data);
 });
