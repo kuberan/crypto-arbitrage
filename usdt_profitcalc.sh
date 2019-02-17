96892a984acdce9aa186e59d882994801f1be407
Kuberan Marimuthu <kuberanmarimuthu@gmail.com>
2019-02-17 14:23:15 -0800

    profitcalc script exclusive for USDT

---
+++ usdt_profitcalc.sh
@@ -0,0 +1,6 @@
+#Assuming that there are not any duplicate transactions
+
+grep 'Net Profit after fee: (' usdt_backtest.file | sort -n | uniq | cut -d" " -f6 > ./usdt_profits.txt
+cat usdt_profits.txt | sort -n
+echo "Total Profit (USDT)"
+cat ./usdt_profits.txt | awk '{s+=$1} END {print s}'
