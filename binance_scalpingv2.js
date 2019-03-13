69ad96cea2d595d0c2f8844974322354c3fb949b
Kuberan Marimuthu <kuberanmarimuthu@gmail.com>
2019-03-12 21:56:43 -0700

    triggerSellBackTrade() Implementation

--- binance_scalpingv2.js
+++ binance_scalpingv2.js
@@ -26,8 +26,8 @@ var checkNextLegInteveralId;
 
 function tradeExtendedArbitrageStrategy(baseCurrency='USDT',
 					buyCurrency='BNB',
-					targetProfitPercent=0.5,
-					stopLossPercent=0.5,
+					targetProfitPercent=1.0,
+					stopLossPercent=1.0,
 					feePercent=0.1)
 {
 	var standardMarkets = ['BTC', 'USDT', 'EOS', 'ETH', 'BNB'];
@@ -62,13 +62,14 @@ function tradeExtendedArbitrageStrategy(baseCurrency='USDT',
                                 console.log("first leg order id: " + response.orderId);
                                 if(response.orderId)
                                 {
-                                        baseCurrencyBalance = 0.0;
+                                        /*baseCurrencyBalance = 0.0;
                                         response.fills.forEach((fill) => {
                                                 baseCurrencyBalance = baseCurrencyBalance + (fill.qty * fill.price);
-                                        });
+                                        });*/
                                         binance.balance((error, balances) => {
                                                 if ( error ) return console.error(error.body);
                                                 buyCurrencyBalance = balances[buyCurrency].available;
+						console.log(buyCurrency + ' balance: ' + buyCurrencyBalance);
                                                 checkNextLegInteveralId = setInterval(checkNextLeg,
 											1000,
 											buyCurrencyBalance,
@@ -103,6 +104,10 @@ function checkNextLeg(buyCurrencyBalance=0.0,
 		var maxProfitPercent = parseFloat(0.0);
 		var minLossPercent = parseFloat(-100.0);
 		var sellBackProfitPercent = parseFloat(0.0);
+		var maxProfitPercentTradeType;
+		var minLossPercentTradeType;
+		var maxProfitPercentSecondLegTradeBuyCurrency;
+		var minLossPercentSecondLegTradeBuyCurrency;
 		Object.keys(tickerData).forEach(function(symbol) {
 			var ticker = tickerData[symbol];
 			var isBuyCurrencyBaseOfSymbol = symbol.indexOf(buyCurrency) == (symbol.length - buyCurrency.length);
@@ -119,15 +124,17 @@ function checkNextLeg(buyCurrencyBalance=0.0,
 			var thirdLegSymbol = symbolBuyCurrency + baseCurrency;
 			var secondLegTradeBalance = buyCurrencyBalance / ticker.askPrice * ((100.0 - feePercent) / 100.0);
 			var thirdLegTradeBalance = secondLegTradeBalance * tickerData[thirdLegSymbol].bidPrice * ((100.0 - feePercent) / 100.0);
-			var profitPercent = parseFloat((thirdLegTradeBalance - baseCurrencyBalance)/(baseCurrencyBalance * ((100.0 - feePercent) / 100.0)) * 100.0).toFixed(2);
+			var profitPercent = parseFloat((thirdLegTradeBalance - baseCurrencyBalance)/baseCurrencyBalance * 100.0).toFixed(2);
 			//console.log(symbol + '->' + thirdLegSymbol);
 			if(profitPercent != Infinity)
 			{
 				if (profitPercent > maxProfitPercent)
 				{
 					maxProfitPercent = parseFloat(profitPercent);
-					console.log(tickerData[symbol]);
-					console.log(tickerData[thirdLegSymbol]);
+					maxProfitPercentTradeType = 'ARBITRAGE';
+					maxProfitPercentSecondLegTradeBuyCurrency = symbolBuyCurrency;
+					//console.log(tickerData[symbol]);
+					//console.log(tickerData[thirdLegSymbol]);
 				}
 				if (profitPercent < maxLossPercent)
 				{
@@ -136,25 +143,95 @@ function checkNextLeg(buyCurrencyBalance=0.0,
 				if(profitPercent < parseFloat(0.0) && profitPercent > minLossPercent)
 				{
 					minLossPercent = parseFloat(profitPercent);
+					minLossPercentTradeType = 'ARBITRAGE';
+					minLossPercentSecondLegTradeBuyCurrency = symbolBuyCurrency;
 				}
 			}
 
 			//Sell Back Trade
 			var sellBackTradeBalance = buyCurrencyBalance * tickerData[buyCurrency + baseCurrency].bidPrice * ((100.0 - feePercent) / 100.0);
-			sellBackProfitPercent = parseFloat((sellBackTradeBalance - baseCurrencyBalance)/(baseCurrencyBalance * ((100.0 - feePercent) / 100.0)) * 100.0).toFixed(2);
+			sellBackProfitPercent = parseFloat((sellBackTradeBalance - baseCurrencyBalance)/baseCurrencyBalance * 100.0).toFixed(2);
 		});
-		console.log('Arbitrage - ' + 'maxProfit ' + maxProfitPercent + '% maxLoss ' + maxLossPercent + '% minLoss' + minLossPercent + '%');
-		console.log('SellBack Profit/Loss: ' + sellBackProfitPercent + '%');
+		//console.log('Arbitrage - ' + 'maxProfit ' + maxProfitPercent + '% maxLoss ' + maxLossPercent + '% minLoss' + minLossPercent + '%');
+		//console.log('SellBack Profit/Loss: ' + sellBackProfitPercent + '%');
 		if(sellBackProfitPercent < parseFloat(0.0) && sellBackProfitPercent > minLossPercent)
 		{
 			minLossPercent = parseFloat(sellBackProfitPercent);
+			minLossPercentTradeType = 'SELLBACK';
 		}
 		else if(sellBackProfitPercent > parseFloat(0.0) && sellBackProfitPercent > maxProfitPercent)
 		{
 			maxProfitPercent = parseFloat(sellBackProfitPercent);
+			maxProfitPercentTradeType = 'SELLBACK';
 		}
 		console.log('Overall maxProfit: ' + maxProfitPercent + '% minLoss: ' + minLossPercent + '%');
+
+		//Check Stop Loss or Target Profit Trigger
+		if(maxProfitPercent >= parseFloat(targetProfitPercent))
+		{
+			clearInterval(checkNextLegInteveralId);
+			if(maxProfitPercentTradeType == 'SELLBACK')
+			{
+				triggerSellBackTrade(baseCurrency, baseCurrencyBalance, buyCurrency, buyCurrencyBalance);
+			}
+			else if(maxProfitPercentTradeType == 'ARBITRAGE')
+			{
+				triggerArbitrageTrade(baseCurrency, maxProfitPercentSecondLegTradeBuyCurrency, buyCurrency);
+			}
+			else{
+				console.log('Invalid maxProfitPercentTradeType');
+			}
+		}
+		else if(minLossPercent <= parseFloat(-stopLossPercent))
+		{
+			clearInterval(checkNextLegInteveralId);
+			if(minLossPercentTradeType == 'SELLBACK')
+                        {
+                                triggerSellBackTrade(baseCurrency, baseCurrencyBalance, buyCurrency, buyCurrencyBalance);
+                        }
+                        else if(minLossPercentTradeType == 'ARBITRAGE')
+                        {
+                                triggerArbitrageTrade(baseCurrency, minLossPercentSecondLegTradeBuyCurrency, buyCurrency);
+                        }
+                        else{
+                                console.log('Invalid minLossPercentTradeType');
+                        }
+		}
 	});
 }
 
-tradeExtendedArbitrageStrategy();
+function triggerArbitrageTrade(baseCurrency, secondLegBuyCurrency, thirdLegSellCurrency)
+{
+	var secondLegMarket = secondLegBuyCurrency + thirdLegSellCurrency;
+	var thirdLegMarket = thirdLegSellCurrency + baseCurrency;
+	console.log('Arbitrage Trade Triggered');
+	console.log('Triggered MARKET Order Buy ' + secondLegMarket);
+	console.log('Triggered MARKET Order Sell ' + thirdLegMarket);
+}
+
+function triggerSellBackTrade(baseCurrency, baseCurrencyBalance, sellCurrency, sellQuantity)
+{
+	console.log('Sellback Trade Triggered');
+	var market = sellCurrency + baseCurrency;
+	console.log('Triggering MARKET order sell ' + market);
+	binance.marketSell(buyCurrency + baseCurrency, sellQuantity, (error, response) => {
+		if(error){ console.log(error.body); return; }
+		console.log("Market Sell response ", response);
+		console.log("Sellback Order id: " + response.orderId);
+		if(response.orderId)
+		{
+			console.log('Triggered MARKET order sell with quantity: ' + sellQuantity + ' ' + sellCurrency);
+			var finalBaseCurrencyBalance;
+			response.fills.forEach((fill) => {
+				finalBaseCurrencyBalance = finalBaseCurrencyBalance + (fill.qty * fill.price);
+			});
+			console.log(baseCurrency + ' balance: ' + finalBaseCurrencyBalance + ' ' + baseCurrency);
+		}
+	});
+}
+
+tradeExtendedArbitrageStrategy(baseCurrency='USDT',
+                                        buyCurrency='BNB',
+                                        targetProfitPercent=0.25,
+                                        stopLossPercent=0.35,
+                                        feePercent=0.1);
